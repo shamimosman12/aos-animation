61dc3c6073a84776ad3746bf2e96db9b1e0bc133
Shamim Hossain <167690026+shamimosman12@users.noreply.github.com>
2024-10-03 18:02:44 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/*--------js-------*/
+//aos
+ AOS.init({
+ 	 offset: 100,
+ 	 delay: 50,
+ 	duration: 500,
+ 	easing: 'linear',
+ });
+
+
+/*--------jq--------*/
+//$(document).ready(function(){
+
+
+//});
